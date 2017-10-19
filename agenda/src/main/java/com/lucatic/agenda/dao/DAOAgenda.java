80a7d969034d8cd74bb7d1c0ba6800023f0946a7
RGD10 <admin@pwc1>
2017-10-19 17:08:25 +0200

    Creado interface dao y daoPesona con sus funciones basicas CRUD.

---
+++ agenda/src/main/java/com/lucatic/agenda/dao/DAOAgenda.java
@@ -0,0 +1,39 @@
+package com.lucatic.agenda.dao;
+
+import java.util.List;
+
+public class DAOAgenda extends IDAO <Agenda,String>{
+
+	@Override
+	public Agenda findById(String key) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Agenda> findAll() {
+		
+		return null;
+	}
+
+	@Override
+	public int delete(Agenda ov) {
+		
+		return 0;
+	}
+
+	@Override
+	public int insert(Agenda ov) {
+		
+		return 0;
+	}
+
+	@Override
+	public int update(Agenda ov) {
+		
+		return 0;
+	}
+
+	
+
+}
