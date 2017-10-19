117af40bf47a3aebf5967edcfb33d2d293e3f536
admin <admin@pwc1>
2017-10-19 17:15:09 +0200

    dtgfty

--- agenda/src/main/java/com/lucatic/agenda/config/AgendaConfig.java
+++ agenda/src/main/java/com/lucatic/agenda/config/AgendaConfig.java
@@ -1,6 +1,10 @@
 package com.lucatic.agenda.config;
 
 import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -33,7 +37,16 @@ public class AgendaConfig {
 			///registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");	    
 		}
 
-	    
+		  @Bean(name = "dataSource")
+		    public DataSource getDataSource() {
+				BasicDataSource dataSource = new BasicDataSource();
+		    	dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		    	dataSource.setUrl("jdbc:mysql://localhost:3306/usersdb");
+		    	dataSource.setUsername("root");
+		    	dataSource.setPassword("root");
+		    	
+		    	return dataSource;
+		    }
 	    private Properties getHibernateProperties() {
 	    	Properties properties = new Properties();
 	    	properties.put("hibernate.show_sql", "true");
